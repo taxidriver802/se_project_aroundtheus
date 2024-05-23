34eb0670302960d92e9bb94c780fb08a09fa453e
taxidriver802 <JAcox12@icloud.com>
2024-05-23 14:14:47 -0500

    fixed array name

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initialcards = [
+const initialCards = [
   {
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -25,4 +25,4 @@ const initialcards = [
   },
 ];
 
-console.log(initialcards);
+console.log(initialCards);
