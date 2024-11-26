2426541af5dd5f26cba5b6c7bf703e608055e697
jason cox <JAcox12@icloud.com>
2024-11-26 02:21:39 -0600

    removed all console.log()

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -7,8 +7,6 @@ export default class Popup {
   }
 
   setEventListeners() {
-    console.log(1);
-
     this.popupElement
       .querySelector(".modal__close")
       .addEventListener("click", this._handleCloseClick);
